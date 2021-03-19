b1209f78d99446750e4980153779f8f9e0810ef0
gabriel-macario <gmacarioweb@gmail.com>
2021-03-18 21:49:32 -0700

    Finish login step

--- cypress/integration/sample.spec.js
+++ cypress/integration/sample.spec.js
@@ -7,15 +7,19 @@ describe('Sample Suite', () => {
     it('Does the given instructions', () => {
         cy.visit('https://www.demoblaze.com')
 
+        // Verify Logged Out state
+        cy.get('#logout2').should('not.be.visible')
+
+        // Register User
         cy.get('#signin2').click()
     
+        // Typing into input is flaky without this wait, possibly due to XHR requests messing up `type` call
         cy.wait(500)
 
         cy.get('#sign-username', { timeout: 10000 })
           .should('be.visible')
           .type(email)
           .should('have.value', email)
-
 
         cy.get('#sign-password', { timeout: 10000 })
             .should('be.visible')
@@ -25,5 +29,27 @@ describe('Sample Suite', () => {
         cy.get('[onclick="register()"]', { timeout: 10000 })
             .should('be.visible')
             .click()
+
+        // Login as User
+        cy.get('#login2').click()
+
+        cy.wait(500)
+
+        cy.get('#loginusername', { timeout: 10000 })
+            .should('be.visible')
+            .type(email)
+            .should('have.value', email)
+
+        cy.get('#loginpassword', { timeout: 10000 })
+            .should('be.visible')
+            .type(password)
+            .should('have.value', password)
+
+        cy.get('[onclick="logIn()"]', { timeout: 10000 })
+            .should('be.visible')
+            .click()
+
+        // Verify Logged In state
+        cy.get('#logout2').should('be.visible')
     })
 })
